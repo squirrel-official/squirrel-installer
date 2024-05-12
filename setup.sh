be360ee6e86e88cf7f6c35924f9b5356c673a33a
squirrel-official <83177270+squirrel-official@users.noreply.github.com>
2024-05-13 02:11:57 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ sudo apt upgrade -y
 # Install system-wide dependencies
 sudo apt install -y python3-pip libjpeg-dev zlib1g-dev libssl-dev libgtk2.0-dev pkg-config ffmpeg libsm6 libxext6 cmake python3-opencv libportaudio2 libatlas-base-dev v4l2loopback-dkms avahi-dnsconfd openjdk-17-jdk libhdf5-dev
 
+sudo pip3 install --upgrade pip
 # Install Python packages
 sudo pip3 install Pillow dlib face_recognition numpy opencv-contrib-python tflite-support tensorflow-aarch64 h5py deepface tf-keras mediapipe facenet-pytorch ultralytics
 
