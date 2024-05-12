17d297078d11d4146e907fff138396edf89515f7
anil <anil.kumar.ait09@gmail.com>
2024-05-13 01:48:00 +1000

    setup

--- setup.sh
+++ setup.sh
@@ -1,40 +1,18 @@
-# Ubuntu 20.04 server 64 bit
-cd /usr/local/
-sudo chmod -R 777 .
+#!/bin/bash
 
+# Update package lists
 sudo apt update
-sudo apt install python3-pip -y
-sudo apt-get install libjpeg-dev zlib1g-dev  -y
-sudo apt-get install libssl-dev -y
-sudo apt-get install libgtk2.0-dev -y
-sudo apt-get install pkg-config -y
-sudo apt-get install ffmpeg libsm6 libxext6  -y
-sudo apt-get update
-sudo apt-get install -y cmake
+sudo apt upgrade -y
+# Install system-wide dependencies
+sudo apt install -y python3-pip libjpeg-dev zlib1g-dev libssl-dev libgtk2.0-dev pkg-config ffmpeg libsm6 libxext6 cmake python3-opencv libportaudio2 libatlas-base-dev v4l2loopback-dkms avahi-dnsconfd openjdk-17-jdk libhdf5-dev
 
-pip3 install Pillow
-pip3 install dlib
-pip3 install face_recognition -v
-pip3 install numpy --upgrade
-pip3 install opencv-contrib-python -v
-sudo apt-get install -y python3-opencv
+# Install Python packages
+sudo -H pip3 install Pillow dlib face_recognition numpy opencv-contrib-python tflite-support tensorflow-aarch64 h5py deepface tf-keras mediapipe facenet-pytorch ultralytics
 
-pip3 install tflite-support
-sudo apt install python3-h5py -y
-pip3 install tensorflow-aarch64
-sudo apt-get install libportaudio2
- sudo apt-get install libatlas-base-dev -y
+# Set permissions (if necessary)
+# sudo chmod -R 777 .
 
-sudo apt-get install v4l2loopback-dkms -y
-sudo apt-get install avahi-dnsconfd -y
-#Added for RaspberryPi
-echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install python3-tflite-runtime
-
-sudo apt-get update -y
-sudo apt-get upgrade -y
-
-
-sudo chmod -R 777 .
+# Install SDKMAN and Gradle
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install gradle
